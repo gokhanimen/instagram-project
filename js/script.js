d99c8a01390863105f8cb1763a3db18d69ff146c
Gökhan İMEN <70821916+gokhanimen@users.noreply.github.com>
2021-10-10 22:49:07 +0300

    Added swiper slider, organized container structure and added post area.

---
+++ js/script.js
@@ -0,0 +1,9 @@
+var mySwiper = new Swiper('.swiper-container',{
+	spaceBetween: 0,
+	slidesPerView: 7,
+  initialSlide: 0,
+	navigation: {
+        nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev'
+	}
+});
