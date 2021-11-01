05567f22a7c2a0b2e5dcac294a39f5abc79a782c
Gökhan İMEN <70821916+gokhanimen@users.noreply.github.com>
2021-11-01 23:40:29 +0300

    The project was divided into partial.

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+    $SERVER_NAME = "localhost";
+    $USER_NAME = "root";
+    $PASSWORD = "";
+    $DATABASE = "instagram_db";
+
+    $connection_string = mysqli_connect($SERVER_NAME, $USER_NAME, $PASSWORD, $DATABASE) or die("MySQL bağlantısı sağlanamadı! Hata: ".mysqli_error($connection_string));
+
+    mysqli_query($connection_string, "SET NAMES UTF8")
+?>
