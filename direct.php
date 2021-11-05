2accd14f268b93a8055c71fa1d62c73ecb7e0fb5
Gökhan İMEN <70821916+gokhanimen@users.noreply.github.com>
2021-11-06 00:09:44 +0300

    header has been added.

--- direct.php
+++ direct.php
@@ -1,7 +1,20 @@
-<?php
-    require_once "./shared/header.php";
-?>
-
+<!DOCTYPE html>
+<html lang="tr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Instagram</title>
+    <link rel="icon" href="./img/instagram-logo-16px.png">
+    <link rel="stylesheet" href="./css/shared/config.css?v=<?=time()?>">
+    <link rel="stylesheet" href="./css/shared/header.css?v=<?=time()?>">
+    <link rel="stylesheet" href="./css/direct.css?v=<?=time()?>">
+</head>
+<body>
+    <?php
+        include "./config.php";
+        include "./header.php";
+    ?>
     <main class="container">
         <section class="main-content">
             <div class="user-flow-area">
@@ -140,7 +153,6 @@
 
                         <div class="user">
                             <img class="user-img" src="./img/people8.jpg" alt="Profile Photo">
-            
                             <div class="user-info">
                                 <a href="#">mark.zuckerberk</a>
                                 <div class="message-info">
